95dc609b7f231c6e1841ebefdbac855020a76f33
Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>
2023-01-03 16:24:17 +0000

    feat: remove UA
    Signed-off-by: Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -336,9 +336,6 @@ export class Brainly {
 				'Sec-Fetch-Mode': 'cors',
 				'Sec-Fetch-Site': 'same-origin',
 				TE: 'trailers',
-				'User-Agent':
-					'Mozilla/5.0 (X11; Linux x86_64; rv:98.0)' +
-					'Gecko/20100101 Firefox/98.0',
 			},
 		});
 	}
